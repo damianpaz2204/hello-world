d9062f19c243609cdf81715dae6e5573ef6afea6
Damian <damian2204@gmail.com>
2025-11-16 21:05:07 -0300

    feature: add frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "system-ui, sans-serif" }}>
+      <h1>Frontend Hello World</h1>
+      <p>Mi primer app React hablando con un backend FastAPI (todavía no, pero ya vamos).</p>
+    </div>
+  );
+}
+
+export default App;
